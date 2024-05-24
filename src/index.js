68a1ccd73db66f8b2d984c9ea13b1b3bd430e281
GodderE2D <55918888+GodderE2D@users.noreply.github.com>
2024-05-23 21:27:45 -0400

    refactor: retry, mobile header size

--- src/index.js
+++ src/index.js
@@ -5,21 +5,27 @@ const notWorking = document.getElementById("notWorking");
 const selectAll = document.getElementById("selectAll");
 const copy = document.getElementById("copy");
 
+let retried = false;
+
 const error = (error) => {
   console.error(error);
 
   debugText.innerText = error;
 
-  statusText.innerText =
-    "Oh no! Couldn't copy a zero-width space to your clipboard.";
+  statusText.innerText = "Oh no! Couldn't copy a zero-width space to your clipboard.";
 
   notWorking.style.display = "none";
   errorDiv.style.display = "block";
+
+  if (error !== "Manually triggered." && (!retried || !document.hasFocus())) {
+    console.log("Retrying in 500ms...");
+    setTimeout(run, 500);
+    retried = true;
+  }
 };
 
 const run = () => {
-  if (!navigator.clipboard)
-    return error("Property navigator.clipboard does not exist");
+  if (!navigator.clipboard) return error("Property navigator.clipboard does not exist");
 
   navigator.clipboard
     .writeText("\u200B")
@@ -34,24 +40,19 @@ const run = () => {
 
 run();
 
-if (navigator.userAgent.indexOf("Win") !== -1) {
-  selectAll.innerHTML =
-    "Click on the text field and press <kbd>Ctrl</kbd> + <kbd>A</kbd>.";
+if (navigator.userAgent.includes("like Mac")) {
+  selectAll.innerHTML = "Press on the text field to focus, tap on the field, then press <code>Select All</code>.";
+  copy.innerHTML = "Press <code>Copy</code>.";
+} else if (navigator.userAgent.includes("Android")) {
+  selectAll.innerHTML = "Press on the text field to focus, long press on it, then press <code>Select All</code>.";
+  copy.innerHTML = "Press <code>Copy</code>.";
+} else if (navigator.userAgent.includes("Win")) {
+  selectAll.innerHTML = "Click on the text field and press <kbd>Ctrl</kbd> + <kbd>A</kbd>.";
   copy.innerHTML = "Press <kbd>Ctrl</kbd> + <kbd>C</kbd>.";
-} else if (navigator.userAgent.indexOf("Mac") !== -1) {
-  selectAll.innerHTML =
-    "Click on the text field and press <kbd>Cmd</kbd> + <kbd>A</kbd>.";
+} else if (navigator.userAgent.includes("Mac")) {
+  selectAll.innerHTML = "Click on the text field and press <kbd>Cmd</kbd> + <kbd>A</kbd>.";
   copy.innerHTML = "Press <kbd>Cmd</kbd> + <kbd>C</kbd>.";
-} else if (navigator.userAgent.indexOf("Linux") !== -1) {
-  selectAll.innerHTML =
-    "Click on the text field and press <kbd>Ctrl</kbd> + <kbd>A</kbd>.";
+} else if (navigator.userAgent.includes("Linux")) {
+  selectAll.innerHTML = "Click on the text field and press <kbd>Ctrl</kbd> + <kbd>A</kbd>.";
   copy.innerHTML = "Press <kbd>Ctrl</kbd> + <kbd>C</kbd>.";
-} else if (navigator.userAgent.indexOf("Android") !== -1) {
-  selectAll.innerHTML =
-    "Press on the text field to focus, then, long press on it, finally, press on <code>Select All</code>.";
-  copy.innerHTML = "Press <code>Copy</code>.";
-} else if (navigator.userAgent.indexOf("like Mac") !== -1) {
-  selectAll.innerHTML =
-    "Press on the text field to focus, then, long press on it, finally, press on <code>Select All</code>.";
-  copy.innerHTML = "Press <code>Copy</code>.";
 }
